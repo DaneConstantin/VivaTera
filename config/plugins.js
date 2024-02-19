ce83fe7095711fe45a27206c98aa9cd05562ffef
DanCoDesign <dancodesign@proton.me>
2024-02-19 10:13:54 +0200

    Cloudinary Setup

--- config/plugins.js
+++ config/plugins.js
@@ -11,5 +11,20 @@ module.exports = ({ env }) => ({
             },
         },
     },
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: env('dfks8op0a'),
+                api_key: env('177422599939182'),
+                api_secret: env('cgqkvpORGMmkUARvLh71EU1QDQM'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        },
+    },
     // ...
 });
